f6e705ff01eff35cae3488684508c191098b29b4
Damien Bradley <damienbradleyDSP@gmail.com>
2020-01-02 21:26:59 +0000

    Improve readibility

--- alias_flipper.py
+++ alias_flipper.py
@@ -1,22 +1,25 @@
 import scipy.signal as sci
 
-def half_nyquist_filter(input_signal,filt_type):
-    if filt_type=='lowpass':
-        h = sci.firwin(101,0.5,window='hamming',pass_zero=True)
-        output = sci.lfilter(h,[1],input_signal)
-    else:
-        h = sci.firwin(101,0.5,window='hamming',pass_zero=False)
-        output = sci.lfilter(h,[1],input_signal) 
-    
+def filter_highpass(input_signal):          # basic low and high pass filters
+    h = sci.firwin(101,0.5,window='hamming',pass_zero=False)
+    output = sci.lfilter(h,[1],input_signal) 
+
+    return output
+
+def filter_lowpass(input_signal):
+    h = sci.firwin(101,0.5,window='hamming',pass_zero=True)
+    output = sci.lfilter(h,[1],input_signal)
+
     return output
 
 def decimate_then_zeropad(signal):
-    downsampled_signal = []
+
+    downsampled_signal = []                         # decimates signal taking 1 in every 2 samples
     for n, sample in enumerate(signal):
         if n%2==0:
                 downsampled_signal.append(sample)
 
-    upsampled_signal = []
+    upsampled_signal = []                           # upsamples back to original rate - freqs are now mirrored in spectrum
     for n,sample in enumerate(downsampled_signal):
         upsampled_signal.append(sample)
         upsampled_signal.append(0)
@@ -25,23 +28,23 @@ def decimate_then_zeropad(signal):
 
 def alias_flip(input_signal):   # flips a signal's frequency components, low frequencies are now high and vice versa
 
-
-    low_passed_original_sr = half_nyquist_filter(input_signal,'lowpass')
+    # Low frequencies become high
+    low_passed_original_sr = filter_lowpass(input_signal)
 
     alias_reflection = decimate_then_zeropad(low_passed_original_sr)
 
-    output_signal = half_nyquist_filter(alias_reflection,'highpass')
+    output_signal = filter_highpass(alias_reflection)
 
     
-
-    high_passed_original_sr = half_nyquist_filter(input_signal,'highpass')
+    # High Frequencies become low
+    high_passed_original_sr = filter_highpass(input_signal)
 
     alias_reflection = decimate_then_zeropad(high_passed_original_sr)
 
-    output_signal2 = half_nyquist_filter(alias_reflection,'lowpass')
+    output_signal2 = filter_lowpass(alias_reflection)
 
     
-
+    # Return combination
     return output_signal + output_signal2
 
 
